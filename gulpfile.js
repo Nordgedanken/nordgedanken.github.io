9d66092895e10e72e9ea79c6ef1537e64c9d810f
MTRNord <mtrnord1@gmail.com>
2018-04-28 18:31:04 +0200

    update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -14,28 +14,28 @@ var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'jekyll';
  * Build the Jekyll Site
  * runs a child process in node that runs the jekyll commands
  */
-gulp.task('jekyll-build', function (done) {
+/*gulp.task('jekyll-build', function (done) {
 	return cp.spawn(jekyllCommand, ['build'], {stdio: 'inherit'})
 		.on('close', done);
-});
+});*/
 
 /*
  * Rebuild Jekyll & reload browserSync
  */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+/*gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
 	browserSync.reload();
-});
+});*/
 
 /*
  * Build the jekyll site and launch browser-sync
  */
-gulp.task('browser-sync', ['jekyll-build'], function() {
+/*gulp.task('browser-sync', ['jekyll-build'], function() {
 	browserSync({
 		server: {
 			baseDir: '_site'
 		}
 	});
-});
+});*/
 
 /*
 * Compile and minify sass
@@ -78,12 +78,12 @@ gulp.task('js', function(){
 		.pipe(gulp.dest('assets/js/'))
 });
 
-gulp.task('watch', function() {
-  gulp.watch('src/styles/**/*.scss', ['sass', 'jekyll-rebuild']);
-  gulp.watch('src/js/**/*.js', ['js']);
-  gulp.watch('src/fonts/**/*.{tff,woff,woff2}', ['fonts']);
-  gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
-  gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-rebuild']);
-});
+//gulp.task('watch', function() {
+//  gulp.watch('src/styles/**/*.scss', ['sass', 'jekyll-rebuild']);
+//  gulp.watch('src/js/**/*.js', ['js']);
+//  gulp.watch('src/fonts/**/*.{tff,woff,woff2}', ['fonts']);
+//  gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
+//  gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-rebuild']);
+//});
 
-gulp.task('default', ['js', 'sass', 'fonts', 'browser-sync', 'watch']);
+gulp.task('default', ['js', 'sass', 'fonts']);
